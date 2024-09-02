4b9fddebddd3826f6b3a4bb685f22e241a44be3f
samuel23lewis <samuel23lewis@gmail.com>
2024-09-02 12:29:51 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+function app(){
+    var fname=document.getElementById("fname").value;
+    var lname=document.getElementById("lname").value;
+    var number=document.getElementById("number").value;
+    alert("Dear " +fname+ " " +lname+ " your application has been submitted");
+}
